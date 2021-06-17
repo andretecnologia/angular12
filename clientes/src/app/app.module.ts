4506c9505a79e8e2b356760cf04e052efcc44ac3
andretecnologia <andre@andretecnologia.com.br>
2021-06-17 16:12:33 -0300

    Initial commit

---
+++ clientes/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
